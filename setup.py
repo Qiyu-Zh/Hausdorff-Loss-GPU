6c31030be8b82798dc781799039ba412ad0ea275
Qiyu Zhang <148504334+Qiyu-Zh@users.noreply.github.com>
2024-09-25 14:40:20 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Hausdorff distance loss in GPU",
+    version="0.1.0",
+    description="A short description of your package",
+    packages=find_packages(),  # Automatically find and include your package
+    install_requires=[         # List of dependencies
+      "torch>=2.3.0",
+      "typing",
+      "py_distance_transforms",
+      "juliacall"
+    ],
+    classifiers=[              # Metadata
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.9',
+)
