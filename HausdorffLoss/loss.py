20ade94059e70666db263c4c1a11a27289fa1af3
Qiyu Zhang <148504334+Qiyu-Zh@users.noreply.github.com>
2024-09-25 16:15:49 -0700

    Update loss.py

--- HausdorffLoss/loss.py
+++ HausdorffLoss/loss.py
@@ -28,23 +28,24 @@ class HD_loss(nn.Module):
         return field
 
     def forward(self, pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        """
-        Uses one binary channel: 1 - fg, 0 - bg
-        pred: (b, c, x, y, z) or (b, c, x, y)
-        target: (b, 1, x, y, z) or (b, 1, x, y)
-        """
+
         if self.apply_nonlin is not None:
             pred = self.apply_nonlin(pred)
         with torch.no_grad():
             target = target.squeeze(1) 
-            if len(pred.shape) == 5:
+            if pred.shape[1] == 1: 
+                pred_dt = self.distance_field(pred_dt > 0.5)
+            elif len(pred.shape) == 5:
                 target = torch.nn.functional.one_hot(target.long(), pred.shape[1]).permute(0, 4, 1, 2, 3)[:,1:]    
                 pred_dt = self.distance_field(torch.nn.functional.one_hot(torch.argmax(pred, dim=1).long(), pred.shape[1]).permute(0, 4, 1, 2, 3)[:,1:].to(dtype=torch.bool))
             elif len(pred.shape) == 4:
                 target = torch.nn.functional.one_hot(target.long(), pred.shape[1]).permute(0, 3, 1, 2)[:,1:]    
                 pred_dt = self.distance_field(torch.nn.functional.one_hot(torch.argmax(pred, dim=1).long(), pred.shape[1]).permute(0, 3, 1, 2)[:,1:].to(dtype=torch.bool))
             target_dt = self.distance_field(target.to(dtype=torch.bool))
-        pred_error = (pred[:,1:] - target) ** 2
+        if pred.shape[1] == 1: 
+            pred_error = (pred - target) ** 2
+        else:
+            pred_error = (pred[:,1:] - target) ** 2
         distance = pred_dt ** self.alpha + target_dt ** self.alpha
 
         dt_field = pred_error * distance
