7385bede03e77846a679b2f89019d53a66687fa5
Qiyu Zhang <148504334+Qiyu-Zh@users.noreply.github.com>
2024-09-25 15:24:37 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Hausdorff distance loss in GPU",
+    name='HausdorffLoss',
     version="0.1.0",
     description="A short description of your package",
     packages=find_packages(),  # Automatically find and include your package
